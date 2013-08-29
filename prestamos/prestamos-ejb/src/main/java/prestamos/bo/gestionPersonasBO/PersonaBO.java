78c22b9d5bb04b542302e08eb32abc984e62ed53
albeirobet <eaar23@hotmail.com>
2013-08-28 20:49:38 -0500

    Creacion Bean AccionPersona

---
+++ prestamos/prestamos-ejb/src/main/java/prestamos/bo/gestionPersonasBO/PersonaBO.java
@@ -0,0 +1,16 @@
+package prestamos.bo.gestionPersonasBO;
+
+
+
+import java.awt.List;
+
+import javax.ejb.Local;
+import javax.faces.model.SelectItem;
+
+
+@Local
+public interface PersonaBO {
+
+	//public List<Departamento> listaDepartamentos ();
+	
+}
--- prestamos/prestamos-war/src/main/java/prestamos/controlador/AccionPersona.java
+++ prestamos/prestamos-war/src/main/java/prestamos/controlador/AccionPersona.java
@@ -1,48 +1,99 @@
 package prestamos.controlador;
 
 import java.io.Serializable;
-import java.util.Date;  
+import java.util.Date;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.faces.model.SelectItem;
 import javax.inject.Named;
 
 @Named
-@ApplicationScoped 
-public class AccionPersona implements Serializable{  
-  
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 4131668062123485276L;
+@ApplicationScoped
+public class AccionPersona implements Serializable {
 
-	private Date date1;  
-      
-    private Date date2;  
-      
-    private Date date3;  
-      
-    public Date getDate1() {  
-        return date1;  
-    }  
-  
-    public void setDate1(Date date1) {  
-        this.date1 = date1;  
-    }  
-  
-    public Date getDate2() {  
-        return date2;  
-    }  
-  
-    public void setDate2(Date date2) {  
-        this.date2 = date2;  
-    }  
-      
-    public Date getDate3() {  
-        return date3;  
-    }  
-  
-    public void setDate3(Date date3) {  
-        this.date3 = date3;  
-    }  
-}  
- 
+	private static final long serialVersionUID = 4131668062123485276L;
+	private String cedula;
+	private String direccion;
+	private Date   fechaNacimiento;
+	private String primerApellido;
+	private String primerNombre;
+	private String segundoApellido;
+	private String segundoNombre;
+	private String telefonoFijo;
+	private String telefonoMovil;
+	private SelectItem comboDepartamentos;
+	
+	public AccionPersona(){
+		cedula= "x";
+		direccion="";
+		fechaNacimiento = new Date();
+		primerApellido="";
+		primerNombre="";
+		segundoApellido="";
+		segundoNombre="";
+		telefonoFijo="";
+		telefonoMovil="";
+		comboDepartamentos = new SelectItem();
+		
+	}
+	
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getPrimerApellido() {
+		return primerApellido;
+	}
+	public void setPrimerApellido(String primerApellido) {
+		this.primerApellido = primerApellido;
+	}
+	public String getPrimerNombre() {
+		return primerNombre;
+	}
+	public void setPrimerNombre(String primerNombre) {
+		this.primerNombre = primerNombre;
+	}
+	public String getSegundoApellido() {
+		return segundoApellido;
+	}
+	public void setSegundoApellido(String segundoApellido) {
+		this.segundoApellido = segundoApellido;
+	}
+	public String getSegundoNombre() {
+		return segundoNombre;
+	}
+	public void setSegundoNombre(String segundoNombre) {
+		this.segundoNombre = segundoNombre;
+	}
+	public String getTelefonoFijo() {
+		return telefonoFijo;
+	}
+	public void setTelefonoFijo(String telefonoFijo) {
+		this.telefonoFijo = telefonoFijo;
+	}
+	public String getTelefonoMovil() {
+		return telefonoMovil;
+	}
+	public void setTelefonoMovil(String telefonoMovil) {
+		this.telefonoMovil = telefonoMovil;
+	}
+	
+	public void MostrarDatos(){
+		System.out.println("Estos son los datos capturados:  "+cedula);
+	}
+	
+}
